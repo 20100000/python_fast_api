4e1a9e9dd190cf7e4843179ee6e1beeb6042f64d
20100000 <tiago_honorio2010@hotmail.com>
2026-06-05 11:49:22 -0300

    fix(dev): add test unauthorized

--- tests/products/test_create.py
+++ tests/products/test_create.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi import status
 
 #Happy
 def test_create_product_success(auth_client):
@@ -24,3 +25,12 @@ def test_create_product_error(auth_client):
     assert response.status_code == 422
     assert response.json()["detail"][0]["msg"] == "Input should be a valid string"
 
+def test_create_product_unauthorized(client):
+    payload = {
+        "name": "Cadeira",
+        "code": "C1110",
+        "description": "cadeira de madeira",
+        "company_id": 1
+    }
+    response = client.post("/products/", json=payload)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
